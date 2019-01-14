c29b49ab486f62f78d6ed8c3c38c410754ca9302
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-01-14 14:29:48 -0600

    starting

--- dummy-data.js
+++ dummy-data.js
@@ -10,7 +10,7 @@ const dummyData = [
     timestamp: "July 17th 2017, 12:42:40 pm",
     comments: [
       {
-        username: "philzcoffee",
+        username: "smallchange",
         text:
           "We've got more than just delicious coffees to offer at our shops!"
       },
