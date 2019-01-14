d2071397b02c37279c597b8818fcb89b433afc27
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-01-14 17:41:00 -0600

    added fontawesome i think

--- instagram/src/Components/SearchBar.js
+++ instagram/src/Components/SearchBar.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import logo from './app-icon2.png';
 
 const SearchBar = () => {
     return (
         <div>SearchBar</div>
+        <div className='searchbar-wrapper'>
+            <div className='nav img-logo'>
+                <img src={logo} alt='insta-logo' />
+                <h1>Instagram</h1>
+            </div>
+            <div className='nav'>
+                <button>
+                    <i className='search'></i>
+                    search
+                </button>
+            </div>
+            <div className="nav-icons">
+                <div className='icons'>
+                    <i className='compass'></i>
+                </div>
+                <div className='icons'>
+                    <i className='heart'></i>
+                </div>
+                <div className='icons'>
+                    <i className='user'></i>
+                </div>
+            </div>
+        </div>
     )
 }
 
